c04403aa9b6596abdeeead24492146040f4890bd
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-02-01 14:28:23 +0200

    2

--- script.js
+++ script.js
@@ -25,49 +25,49 @@
 
 // ! способ с рекурсией
 
-// const arr = [1, 2, [1, 2, 3, 4], 3, [1, 2], 4, 5];
+const arr = [1, 2, [1, 2, 3, 4], 3, [1, 2], 4, 5];
 
-// function generateList(arr) {
-//     const ul = document.createElement('ul');
+function generateList(arr) {
+    const ul = document.createElement('ul');
 
-//     arr.forEach(item => {
-//         const li = document.createElement('li');
-//         Array.isArray(item) ? li.append(generateList(item)) : li.innerText = item;
-//         ul.append(li);
-//     });
-//     return ul;
-// }
+    arr.forEach(item => {
+        const li = document.createElement('li');
+        Array.isArray(item) ? li.append(generateList(item)) : li.innerText = item;
+        ul.append(li);
+    });
+    return ul;
+}
 
-// document.querySelector('body').append(generateList(arr));
+document.querySelector('body').append(generateList(arr));
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!
 
-const arrayNumber = [1, 2, [1, 2, 3, 4], 3, [1, 2], 4, 5];
+// const arrayNumber = [1, 2, [1, 2, 3, 4], 3, [1, 2], 4, 5];
 
-function generateList(array2) {
-    const ul = document.createElement("ul");
+// function generateList(array2) {
+//     const ul = document.createElement("ul");
 
-    array2.forEach((i) => {
-        if (Array.isArray(i)) {
-            const liSecond = document.createElement("li");
-            const ulSecond = document.createElement("ul");
+//     array2.forEach((i) => {
+//         if (Array.isArray(i)) {
+//             const liSecond = document.createElement("li");
+//             const ulSecond = document.createElement("ul");
 
-            i.forEach(j => {
-                const liThird = document.createElement("li");
-                liThird.innerText = j;
+//             i.forEach(j => {
+//                 const liThird = document.createElement("li");
+//                 liThird.innerText = j;
 
-                ulSecond.append(liThird);
-                liSecond.append(ulSecond);
-                ul.append(liSecond);
-            })
-        } else {
+//                 ulSecond.append(liThird);
+//                 liSecond.append(ulSecond);
+//                 ul.append(liSecond);
+//             })
+//         } else {
 
-            const li = document.createElement("li");
-            li.innerText = i;
-            ul.append(li);
-        }
-    })
-    document.body.append(ul);
-};
-generateList(arrayNumber);
+//             const li = document.createElement("li");
+//             li.innerText = i;
+//             ul.append(li);
+//         }
+//     })
+//     document.body.append(ul);
+// };
+// generateList(arrayNumber);
 
